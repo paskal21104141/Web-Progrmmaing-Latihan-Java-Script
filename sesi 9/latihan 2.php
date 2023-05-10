80ec4c2f06605aa4885c6e3d8f6e5edcfb92c2cf
Paskalis Ngongo Nuban <127914198+paskal21104141@users.noreply.github.com>
2023-05-10 12:19:31 +0800

    Add files via upload

---
+++ sesi 9/latihan 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+
+<div id="pesan">kita isi</div>
+<span id = "pesan2"></span>
+
+    <script>
+        document.write("Bel<strong>ajar</strong> Memahami<br>");
+        document.write("~~~~~~~~~~~~~~~~~~~~~~~");
+        document.getElementById("pesan").innerHTML = "Diganti Dengan Apa<sup> Saja</sup> Boleh";
+        document.getElementById("pesan2").innerHTML = "Diganti Dengan Apa<sup> Saja</sup> Boleh";
+    </script>
+
+</body>
+</html>
---
+++ sesi 9/latihan 3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+
+    <div>
+        Nama Lengkap
+        <input type="text" id = "txNAMA">
+        </div>
+        <div>
+            <button type="button" onclick="checknama()">CheckNama</button>
+        </div>
+    </form>
+    <script>
+        function checknama(){
+            let nm = document.getElementById("txNAMA").value;
+            alert("Isi Dari field Nama : "+nm);
+        }
+        </script>
+
+</body>
+</html>
